ef3fd41d6d840759d71af0b4c2f6198c03364d1f
salamanderctesiphon <swbrookshire@gmail.com>
2023-10-03 15:14:48 -0500

    edited the server code

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 var cors = require('cors');
-const stripe = require('stripe')('sk_test_51LnUKJDM1jwCEz8OJG69szv032rIo4X0WrFMaXrqxu9g8fdohsL1y54JEUhFUKrqoBquVjN3AzpIFyrbN915bgcd00O5hqoGCJ');
+const stripe = require('stripe')(process.env.STRIPE_PAYMENT_SECRET_KEY);
 
 const app = express();
 app.use(cors());
@@ -8,45 +8,29 @@ app.use(express.static("public"));
 app.use(express.json());
 
 app.post("/checkout", async (req, res) => {
-    /*
-    req.body.items
-    [
-        {
-            id: 1,
-            quantity: 3
-        }
-    ]
 
-    stripe wants
-    [
-        {
-            price: 1,
-            quantity: 3
-        }
-    ]
-    */
-    console.log(req.body);
-    const items = req.body.items;
-    let lineItems = [];
-    items.forEach((item)=> {
-        lineItems.push(
-            {
-                price: item.id,
-                quantity: item.quantity
-            }
-        )
-    });
+  console.log(req.body);
+  const items = req.body.items;
+  let lineItems = [];
+  items.forEach((item)=> {
+    lineItems.push(
+      {
+        price: item.id,
+        quantity: item.quantity
+      }
+    )
+  });
 
-    const session = await stripe.checkout.sessions.create({
-        line_items: lineItems,
-        mode: 'payment',
-        success_url: "http://localhost:3000/success",
-        cancel_url: "http://localhost:3000/cancel"
-    });
+  const session = await stripe.checkout.sessions.create({
+    line_items: lineItems,
+    mode: 'payment',
+    success_url: "http://localhost:5173/success",
+    cancel_url: "http://localhost:5173/cancel"
+  });
 
-    res.send(JSON.stringify({
-        url: session.url
-    }));
+  res.send(JSON.stringify({
+    url: session.url
+  }));
 });
 
 app.listen(4000, () => console.log("Listening on port 4000!"));
